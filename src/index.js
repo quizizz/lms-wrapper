eeebe48ae8ee366728a88c3268142cdba1d92534
Shivam Tripathi <shivamtripathi0108@gmail.com>
2021-02-13 19:38:43 +0530

    feat(dev/canvas): Expose canvas

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
 const GCL = require('./gcl');
 const Edmodo = require('./edmodo');
 const LMSError = require('./error');
+const Canvas = require('./Canvas');
 
 module.exports = {
-  GCL, LMSError, Edmodo,
+  GCL, LMSError, Edmodo, Canvas,
 };
