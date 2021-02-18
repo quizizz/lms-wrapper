0dd0b277f914c390c31b90232441fd3a5f08c0c2
Shivam Tripathi <shivamtripathi0108@gmail.com>
2021-02-18 13:29:06 +0530

    feat(dev/canvas): Collect paginated results

--- src/helpers/utils.d.ts
+++ src/helpers/utils.d.ts
@@ -2,3 +2,7 @@ import { MomentInput } from 'moment';
 
 export function addSeconds(time: MomentInput, seconds: number): number;
 export function addWeeks(date: Date, week?: number): number;
+interface LMS {
+  makeRequest<T>(req: RequestConfig, retry?: number): T;
+}
+export function paginatedCollect<T>(lms: LMS, req: RequestConfig): Promise<T[]>;
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,5 +1,6 @@
 /// <reference path='./utils.d.ts' />
 const moment = require('moment');
+const _ = require('lodash');
 
 exports.addSeconds = (time, seconds) => {
   return moment(time).add(Number(seconds), 'seconds').valueOf();
@@ -8,3 +9,20 @@ exports.addSeconds = (time, seconds) => {
 exports.addWeeks = (date, week = 1) => {
   return moment(date.valueOf()).add(week, 'weeks').valueOf();
 };
+
+exports.paginatedCollect = async (lms, requestConfig) => {
+  const results = [];
+  let page = 1;
+  while (true) {
+    const result = await lms.makeRequest({
+      query: { page, ...requestConfig.query },
+      ...requestConfig,
+    });
+    if (!result || _.isEmpty(result)) {
+      break;
+    }
+    page++;
+    results.push(result);
+  }
+  return results;
+}
