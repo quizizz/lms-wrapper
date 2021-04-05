421c67aa80cf97a1e51e564b0ddfebb9dc7aadcc
Sarthak Saini <echo8795@gmail.com>
2021-04-05 06:22:02 +0530

    feat: added support for schoology oAuth

---
+++ src/oauth.js
@@ -0,0 +1,205 @@
+// / <reference path='./oauth.d.ts' />
+
+const { URL, URLSearchParams } = require('url');
+
+const _ = require('lodash');
+const axios = require('axios').default;
+const { v4: uuidV4 } = require('uuid');
+
+class OAuth {
+	constructor( { 
+		consumerKey = '', 
+		consumerSecret = '', 
+		apiBase = '', 
+		authRealm = '',
+		signatureMethod = 'PLAINTEXT',
+		nonceLength = 16,
+		requestToken,
+		accessToken,
+		errorHandler = (() => {})
+	} ) {
+		this.consumerKey = consumerKey;
+		this.consumerSecret = consumerSecret;
+		this.apiBase = apiBase;
+		this.authRealm = authRealm;
+		this.signatureMethod = signatureMethod;
+		this.nonceLength = nonceLength;
+		this.requestToken = { 
+			token: requestToken.token || '', 
+			secret: requestToken.secret || '', 
+			expiresAt: requestToken.expiresAt || new Date()
+		};
+		this.accessToken = {
+			token: accessToken.token || '', 
+			secret: accessToken.secret || '', 
+			expiresAt: accessToken.expiresAt || new Date()
+		};
+		this.errorHandler = errorHandler;
+	}
+
+	static getTimeStamp () {
+		return parseInt(new Date().getTime()/1000, 10);
+	}
+
+	static getNonce( nonceLength ) {
+		return uuidV4().replace(/-/g, '').slice(-16);
+	}
+
+	static makeURL( apiURL, path, query ) {
+		const url = new URL(apiURL);
+
+		if (path) {
+			url.pathname = path;
+		}
+
+		url.search = new URLSearchParams(query);
+		return url.toString();
+	}
+
+	static post(host, path, query, data, headers = {}) {
+		const url = OAuth.makeURL(host, path, query);
+
+		return axios.post(url, data, {
+			headers,
+			responseType: 'json',
+		});
+	}
+
+	static get(host, path, query, headers) {
+		const url = OAuth.makeURL(host, path, query);
+
+		return axios.get(url, {
+			headers,
+			responseType: 'json',
+		});
+	}
+
+	static jsonifyResponseString(responseString) {
+		const strSplits = responseString.split( '&' );
+
+		return _.reduce( strSplits, ( result, keyValPair ) => {
+			const splits = keyValPair.split( '=' );
+			
+			result[ splits[0] ] = splits[1];
+			return result;
+		}, {} );
+	}
+
+
+	async getRequestTokens( apiPath ) {
+		const oAuthDetails = this.getOAuthDetails();
+		const requestedAt = Date.now();
+
+		try {
+			const response = await OAuth.get(this.apiBase, apiPath, oAuthDetails);
+
+			const jsonified = OAuth.jsonifyResponseString(response.data);
+			const ttl = parseInt( jsonified[ 'xoauth_token_ttl' ] ) || 3600;
+			debugger;
+			return {
+				success: true,
+				response: {
+					token: jsonified[ 'oauth_token' ],
+					secret: jsonified[ 'oauth_token_secret' ],
+					expiresAt: new Date( requestedAt + ( ttl * 1000 ) )
+				}
+			}
+		} catch ( error ) {
+			debugger;
+			this.errorHandler(error);
+		}
+	}
+
+	async getAccessTokens( apiPath ) {
+		const oAuthDetails = this.getOAuthDetails();
+
+		try {
+			const response = await OAuth.get(this.apiBase, apiPath, oAuthDetails);
+			debugger;
+			const jsonified = OAuth.jsonifyResponseString(response.data);
+      		this.accessToken = {
+				token: jsonified[ 'oauth_token' ],
+				secret: jsonified[ 'oauth_token_secret' ],
+			};
+
+			return {
+				success: true,
+				response: this.accessToken
+			}
+		} catch (error) {
+			debugger;
+			this.errorHandler(error);
+		}
+	}
+
+	/**
+	 * Makes a request, defined by the requestConfig, to the server
+	 * Attempts to refresh the accessToken if server throws a "token expired" error and
+	 * then re-attempts the request
+	 */
+	async makeRequest(requestConfig, retries = 0) {
+		try {
+			if (_.isEmpty(this.accessToken)) {
+				debugger;
+				// this.errorHandler( {} )
+				return;
+			}
+
+			const url = OAuth.makeURL(this.apiBase, requestConfig.url, requestConfig.query || {});
+			const oAuthHeader = this.getOAuthHeader();
+
+			debugger;
+
+			const response = await axios({
+				...requestConfig,
+				url,
+				headers: { 
+					...oAuthHeader, 
+					...requestConfig.headers 
+				},
+			});
+			debugger;
+			const { data, status } = response;
+			
+			return { data, status };
+		} catch (err) {
+			debugger;
+		}
+	}
+
+	getOAuthDetails() {
+		const timestamp = OAuth.getTimeStamp();
+		const nonce = OAuth.getNonce( this.nonceLength );
+		const token = this.accessToken.token || this.requestToken.token || '';
+		const secret = this.accessToken.secret || this.requestToken.secret || '';
+		const oAuthConfig = {
+			'oauth_version': '1.0',
+			'oauth_nonce': nonce,
+			'oauth_timestamp': timestamp,
+			'oauth_signature_method': 'PLAINTEXT'
+		};
+
+		if ( !_.isEmpty( this.consumerKey ) ) {
+			oAuthConfig[ 'oauth_consumer_key' ] = this.consumerKey;
+		}
+
+		if ( !_.isEmpty( token ) ) {
+			oAuthConfig[ 'oauth_token' ] = token;
+		}
+		
+		if ( !_.isEmpty( this.consumerSecret ) || !_.isEmpty(secret) ) {
+			oAuthConfig[ 'oauth_signature' ] = `${this.consumerSecret}&${secret}`;
+		}
+
+		return oAuthConfig;
+	}
+
+	getOAuthHeader() {
+		const oAuthDetails = this.getOAuthDetails();
+		const headerParts = _.map(oAuthDetails, (value, key) => `${key}=${value}`);
+		
+		return { Authorization: `OAuth realm="${this.authRealm}", ${headerParts.join(', ')}` };
+	}
+}
+
+module.exports = OAuth;
