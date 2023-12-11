2ee32a81f68613aa24ad9f490f06a79730e1de78
Rohit Bhosle <rohit.bhosle@quizizz.com>
2023-12-11 13:51:50 +0530

    fix: removing the reference of deleted file

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,5 @@
 import GCL from './gcl';
 import LMSError from './error';
-import Edmodo from './edmodo';
 import Canvas from './canvas';
 import Schoology from './schoology';
-export { GCL, LMSError, Edmodo, Canvas, Schoology };
+export { GCL, LMSError, Canvas, Schoology };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 
 const GCL = require('./gcl');
-const Edmodo = require('./edmodo');
 const LMSError = require('./error');
 const Canvas = require('./canvas');
 const Schoology = require("./schoology");
 
 module.exports = {
-  GCL, LMSError, Edmodo, Canvas, Schoology
+  GCL, LMSError, Canvas, Schoology
 };
